dfdcb2e582472f74ea4a3debc9f9b6123089cd0f
muhammedalfaris <muhammedalfariz8003@gmail.com>
2025-10-31 18:20:26 +0530

    base url updation 2

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'; 
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://expense-tracker-backend-zteg.onrender.com'; 
 
 const apiClient = axios.create({
   baseURL: `${baseURL}/api`, 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import {redirect} from 'next/navigation';
 
 export default  function Landing(){
-  redirect('/register')
+  redirect('/login')
 }
